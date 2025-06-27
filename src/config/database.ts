ca95c6413f92e2a7d602a51ee13e0c1c8dd46caa
hitenthanki <hiten.thanki@esparkbizmail.com>
2025-06-27 21:01:28 +0530

    few changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -18,8 +18,7 @@ import { Permission } from '../models/Permission';
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USERNAME as string, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: 24506, 
+  host: process.env.DB_HOST, 
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
